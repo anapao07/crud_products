ce88086b315c6f0005c2da119d7b23ac2c9041bd
anapao07 <annapao.arcos@gmail.com>
2020-07-24 22:05:09 -0500

    Model and Migrations Created

--- app_products/models.py
+++ app_products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    nombre = models.CharField(max_length=60)
+    precio = models.IntegerField()
+    cantidad = models.IntegerField()
+    descripcion = models.CharField(max_length=200)
+    imagen = models.ImageField()
+
+    def __str__(self):
+        return self.nombre
+        
