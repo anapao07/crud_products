97944a8e7651daecd6646fc72d264f5545e1ef69
anapao07 <annapao.arcos@gmail.com>
2020-07-25 20:57:33 -0500

    Function To Create Product

--- app_products/views.py
+++ app_products/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rest_framework import viewsets
 from .models import Products
 from .serializers import ProductsSerializer
+from .forms import ProductsForm
+
 
 def index(request):
-    return render(request,'app_products/index.html')
+    return render(request, 'app_products/index.html')
 
+
+def create(request):
+    form = ProductsForm()
+    if request.method == 'POST':
+        form = ProductsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    context = {
+        'form': form
+    }
+    return render(request, 'app_products/create.html', context)
 
 
 class ProductsViewSet(viewsets.ModelViewSet):
