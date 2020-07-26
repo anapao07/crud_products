e3b3c251ada765d488001f7ed947f8f01b7d4219
anapao07 <annapao.arcos@gmail.com>
2020-07-25 22:01:55 -0500

    Modified Models

--- app_products/models.py
+++ app_products/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Products(models.Model):
     nombre = models.CharField(max_length=60)
+    codigo = models.CharField(max_length=7, unique=True)
     precio = models.IntegerField()
     existencia = models.IntegerField()
     descripcion = models.CharField(max_length=200)
